c48379c378e559c0e4d727920168343fab9af253
Googly <G00glyM00gly@yandex.ru>
2024-08-05 20:36:38 +0300

    First commit

---
+++ Class1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        string name, sec_name;
+//        name = Interaction.InputBox("Введите ваше имя", "Знакомство");
+//        sec_name = Interaction.InputBox("Enter your surname", "acquaintance");
+
+//        string txt = "Your name is " + name +" "+ sec_name + "!";
+
+//        MessageBox.Show(txt, "Uraaaaa", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+//    }
+
+//}
+
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        string name, age;
+
+//        name = Interaction.InputBox("What is your name?", "Tell me");
+//        age = Interaction.InputBox("How old are you?", "And");
+//        MessageBox.Show("Your name is " + name + " and your age is " + age);
+
+//    }
+//}
+
+
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        string name, age;
+
+//        Console.Write("Tell me your name: ");
+//        name = Console.ReadLine();
+//        Console.Write("And your age: ");
+//        age = Console.ReadLine();
+//        Console.Write("Your name is {0,20} and yout age is {1,20}", name, age);
+//        Console.Read();
+//    }
+//}
+
+
+
+//class Programm
+//{
+//    static void Main()
+//    {
+//        int a, b;
+//        string buf;
+//        Console.Write("Enter a: ");
+//        buf = Console.ReadLine();
+//        a = Int32.Parse(buf);
+//        Console.Write("Enter b: ");
+//        buf = Console.ReadLine();
+//        b = Int32.Parse(buf);
+
+//        int c = a + b;
+//        Console.Write(a + b);
+//        Console.Read();
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
